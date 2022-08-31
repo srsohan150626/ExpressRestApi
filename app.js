e2d1dfe173f8061909e80422c83f2df059acc4b5
srsohan150626 <sohan.ice.pust@gmail.com>
2022-08-31 17:56:27 +0600

    security middleware implementation

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const router = require("./src/routes/api");
 const app = new express();
+
+//security middleware import
+const expressRateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const expressMongoSanitize = require('express-mongo-sanitize')
+const xssClean = require('xss-clean')
+const hpp = require('hpp')
+const cors = require('cors')
+
+
+//security middleware implementation
+app.use(cors())
+app.use(expressRateLimit())
+app.use(helmet())
+app.use(expressMongoSanitize())
+app.use(xssClean())
+app.use(hpp())
+
+//Request Rate limiting
+const limiter = expressRateLimit({
+    windowMs: 15*60*1000, // 15 miniutes
+    max: 100 //limit each ip to 100 request per window
+})
+app.use(limiter);
+
 app.use("/api/v1", router);
 
+//undefined route
+app.use('*', (req, res) => {
+    res.status(404).json({success: 'failed', message: 'Undefined route!'});
+})
 module.exports = app;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require("./app");
+const dotenv = require('dotenv');
+dotenv.config({path:'./config.env'})
 
-app.listen(5000, function () {
-    console.log("Server run successfully!");
+app.listen(process.env.PORT, function () {
+    console.log("Server run successfully on port "+process.env.PORT);
 })
