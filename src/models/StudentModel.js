bc0fcde9eb2536a97938ae20c8ec9f7cef4a18ec
srsohan150626 <sohan.ice.pust@gmail.com>
2022-09-28 13:02:21 +0600

    mongoose validation

--- src/models/StudentModel.js
+++ src/models/StudentModel.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 
 const DataSchema = mongoose.Schema({
     name: String,
-    roll: String,
+    roll: {type: Number, required: true, unique: true
+       },
     class: String,
-    remarks: String
-});
+    mobile: {
+        type:String,
+        validate:{
+            validator: function (value) {
+                return /^(?:\+88|88)?(01[3-9]\d{8})$/.test(value)
+            },
+            message:"Invalid Bangladeshi mobile number"
+        }
+    },
+    remarks: {type: String, default: "No Remarks"}
+}, {versionKey:false});
 const studentModel = mongoose.model("students", DataSchema);
 module.exports = studentModel;
